cabe2417eece14fe8bfb1e6458ceea0222df8e92
Stephan Pitois <spitois@gmail.com>
2021-04-23 13:55:23 -0400

    Layout

--- src/components/ProductionCard.svelte
+++ src/components/ProductionCard.svelte
@@ -6,11 +6,14 @@
 
 <style>
   .amp-production-card > a {
+    @apply border-0 border-t border-solid border-gray-200 pt-4 mt-4;
+  }
+  /* .amp-production-card > a {
     @apply border-0 border-b border-solid border-gray-200 pb-4 mb-4;
   }
   .amp-production-card:last-child > a {
     @apply border-b-0 pb-0 mb-0;
-  }
+  } */
 </style>
 
 <article class="amp-production-card">
@@ -21,25 +24,27 @@
   >
     <div class="flex flex-col md:flex-row">
       <div class="w-full md:(w-7/12)">
-        <h2 class="mt-0 mb-1 font-semibold text-gray-800 text-xl leading-none md:(text-xl leading-none)">
+        <h2 class="mt-0 mb-0 font-semibold text-gray-800 text-xl leading-none md:(text-xl leading-none)">
           {production.title}
         </h2>
-        {#if production.startDate}
-          <dl class="text-sm my-1 leading-4">
-            <dt class="sr-only">Dates</dt>
-            <dd class="ml-0 text-gray-800">
-              {formatRange(production.startDate, production.endDate)}
-            </dd>
-          </dl>
-        {/if}
-        {#if production.startPrice}
-          <dl class="text-sm my-1 leading-4">
-            <dt class="sr-only">Tickets</dt>
-            <dd class="ml-0 text-gray-500">
-              {formatRange(production.startPrice, production.endPrice, '$')}
-            </dd>
-          </dl>
-        {/if}
+        <div class="flex flex-row">
+          {#if production.startDate}
+            <dl class="text-sm mt-1 mb-2 leading-4 w-2/3">
+              <dt class="sr-only">Dates</dt>
+              <dd class="ml-0 text-gray-500">
+                {formatRange(production.startDate, production.endDate)}
+              </dd>
+            </dl>
+          {/if}
+          {#if production.startPrice}
+            <dl class="text-sm mt-1 mb-2 leading-4 w-1/3 text-right">
+              <dt class="sr-only">Tickets</dt>
+              <dd class="ml-0 text-gray-500">
+                {formatRange(production.startPrice, production.endPrice, '$')}
+              </dd>
+            </dl>
+          {/if}
+        </div>
       </div>
       <div class="w-full md:(w-5/12)">
         <img
